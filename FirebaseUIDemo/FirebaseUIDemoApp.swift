53b41c7b0215c6b14dbecb0b241762e1453a1746
taise-hikawa <cmmkarubi@gmail.com>
2022-11-24 20:01:39 +0900

    Initial Commit

---
+++ FirebaseUIDemo/FirebaseUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseUIDemoApp.swift
+//  FirebaseUIDemo
+//
+//  Created by 樋川大聖 on 2022/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
